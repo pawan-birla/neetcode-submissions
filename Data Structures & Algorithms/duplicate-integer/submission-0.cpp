c491ced37eab93241ae615b565ad8047af8fb6be
pawan-birla <157245540+pawan-birla@users.noreply.github.com>
2026-08-11 01:19:21 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        set<int> st;
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+        }
+        if(n>st.size()){return true;}
+        return false;
+    }
+};
