ee6615e6885a2a34c9c512138a791e003119380e
pawan-birla <157245540+pawan-birla@users.noreply.github.com>
2026-08-11 02:38:40 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    
+        unordered_multiset<char> ms;
+        if(s.size() != t.size())return false;
+
+        for(int i=0; i<s.size(); i++){
+            ms.insert(s[i]);
+        }
+
+        for(int i=0; i<t.size(); i++){
+            auto it = ms.find(t[i]);
+            if(it == ms.end())return false;
+            else{
+                ms.erase(it);
+            }
+        }
+        return true;
+    
+    
+    
+    
+    
+    
+    
+    
+        // if(s.size() != t.size())return false;
+
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        // for(int i=0; i<s.size(); i++){
+        //     if(s[i] != t[i])return false;
+        // }
+        // return true;
+    }
+};
